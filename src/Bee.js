34163a036a6b97014deb13c64f6d3711d32b16dc
meijiao <meijiao@gmail.com>
2016-09-20 05:47:24 -0700

    Solved all prompts

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+	Grub.call(this);  //inherits grub properties --> grub is a superclass
+	this.age = 5;
+	this.color = "yellow";
 };
+ 
+Bee.prototype = Object.create (Grub.prototype); //failed method lookups on Bee will delegate to Grub
 
+Bee.prototype.job = "keep on growing";
+ 
+Bee.prototype.constructor = Bee;  //When lookups from a new instance of Bee to Bee prototype fails, it will stop and report the right constructor
